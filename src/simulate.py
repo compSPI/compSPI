c674d0e12cd141cc280184152b256a6ae783bf27
Geoffrey Woollard <geoffwoollard@gmail.com>
2021-04-19 17:36:49 -0400

    simulate_atoms, and batch in simulate_slice

--- src/simulate.py
+++ src/simulate.py
@@ -4,7 +4,8 @@ import pandas as pd
 from scipy.ndimage import map_coordinates
 import numba
 
-def simulate(map_r,psize,n_particles,snr,N_crop,
+def simulate_slice(map_r,psize,n_particles,N_crop,snr,do_snr=True,
+            do_ctf=True,
             df_min=15000,
             df_max=20000,
             df_diff_min=100,
@@ -31,49 +32,120 @@ def simulate(map_r,psize,n_particles,snr,N_crop,
     idx_z0 = xyz[:,-1] == 0 
     xy0 = xyz[idx_z0]
 
-
-    np.random.seed(random_seed)
-    qs = coords.get_random_quat(n_particles)
-    Rs = coords.quaternion_to_R(qs.T)
-
-    CTFs, df1s, df2s, df_ang_deg = transfer.random_ctfs(N=N,
-                                psize=psize,
-                                n_particles=n_particles,
-                                df_min=df_min,
-                                df_max=df_max,
-                                df_diff_min=df_diff_min,
-                                df_diff_max=df_diff_max,
-                                df_ang_min=df_ang_min,
-                                df_ang_max=df_ang_max,
-                                kv=kv,
-                                cs=cs,
-                                ac=ac,
-                                phase=phase,
-                                bf=bf,
-                                do_log=do_log
-                                )
-
+    if random_seed is not None: np.random.seed(random_seed)
+    Rs = coords.uniform_rotations(n_particles)
 
     proj_f = np.zeros((n_particles,N,N),dtype=np.complex64)
     for idx in range(n_particles):
-      if do_log and idx % max(1,(n_particles//10)) == 0: print(idx)
-      R = Rs[:,:,idx]
-      xy0_rot = R.dot(xy0.T).T
-      proj_f[idx] = (map_coordinates(map_f.real, xy0_rot.T + N//2,order=1).astype(np.complex64) + 1j*map_coordinates(map_f.imag, xy0_rot.T + N//2,order=1).astype(np.complex64)).reshape(N,N) # important to keep order=1 for speed. linear is good enough
-      proj_f[idx] *= CTFs[idx]
+        if do_log and idx % max(1,(n_particles//10)) == 0: print(idx)
+        R = Rs[:,:,idx]
+        xy0_rot = R.dot(xy0.T).T
+        proj_f[idx] = (map_coordinates(map_f.real, xy0_rot.T + N//2,order=1).astype(np.complex64) + 1j*map_coordinates(map_f.imag, xy0_rot.T + N//2,order=1).astype(np.complex64)).reshape(N,N) # important to keep order=1 for speed. linear is good enough
+
+    if do_ctf:
+        CTFs, df1s, df2s, df_ang_deg = transfer.random_ctfs(N=N,
+                                    psize=psize,
+                                    n_particles=n_particles,
+                                    df_min=df_min,
+                                    df_max=df_max,
+                                    df_diff_min=df_diff_min,
+                                    df_diff_max=df_diff_max,
+                                    df_ang_min=df_ang_min,
+                                    df_ang_max=df_ang_max,
+                                    kv=kv,
+                                    cs=cs,
+                                    ac=ac,
+                                    phase=phase,
+                                    bf=bf,
+                                    do_log=do_log
+                                    )
+
+
+    
+        proj_f *= CTFs
 
     i,f = N//2-N_crop//2, N//2+N_crop//2
-    proj_r = np.zeros((n_particles,N_crop,N_crop))
-    for idx in range(n_particles):
-      proj_r[idx] = fourier.do_ifft(proj_f[idx,i:f,i:f],d=2)
+    proj_r = fourier.do_ifft(proj_f[:,i:f,i:f],d=2,batch=True)
     psize_crop = psize*N/N_crop
 
-    signal = np.std(proj_r)
-    noise = signal/snr
-    proj_r_noise = np.random.normal(loc=proj_r,scale=noise)
+    if do_snr:
+        signal = np.std(proj_r)
+        noise = signal/snr
+        proj_r_noise = np.random.normal(loc=proj_r,scale=noise)
+    else:
+        proj_r_noise = proj_r
 
     meta_data_df = pd.DataFrame({'N':N_crop,'psize':psize_crop,'snr':snr,'df1_A':df1s,'df2_A':df2s,'df_ang_deg':df_ang_deg,'kev':kv,'ac':ac,'cs_mm':cs,
               'rotation_quaternion':[np.array2string(q) for q in qs]
             })
 
     return(proj_r,proj_r_noise,meta_data_df)
+
+def simulate_atoms(atoms,N,psize,n_particles,
+    sigma=1,# in pixels
+    n_trunc=None,# in pixels
+    snr=0.1,
+    do_snr=True,
+    do_ctf=True,
+    df_min=15000,
+    df_max=20000,
+    df_diff_min=100,
+    df_diff_max=500,
+    df_ang_min=0,
+    df_ang_max=360,
+    kv=300,
+    cs=2.0,
+    ac=0.1,
+    phase=0,
+    bf=0,
+    do_log=True,
+    random_seed=0
+    ):
+    assert atoms.shape[0] == 3
+    n_atoms = atoms.shape[-1]
+    assert N%2 == 0, 'even pixel length'
+
+    xy = coords.coords_n_by_d(np.arange(-N//2,N//2),d=2)
+    if n_trunc is None: n_trunc = 6*sigma
+
+    if random_seed is not None: np.random.seed(random_seed)
+    Rs, qs = coords.uniform_rotations(n_particles)
+    g_2d_gpu = gauss_forward_model.make_proj_gpu(atoms/psize,xy,N,n_particles,sigma,n_trunc,Rs) # TODO make general for psize (work in pixel units by converting atoms to pixel units)
+    projs = g_2d_gpu.copy_to_host().reshape(n_particles,N,N)
+
+    # CTF
+    # to avoid CTF aliasing, ensure N is large
+    if do_ctf:
+        projs_f = fourier.do_fft(projs,d=2,batch=True) # TODO: may need to zero pad here for CTF
+        CTFs, df1s, df2s, df_ang_deg = transfer.random_ctfs(N=N,
+                                  psize=psize,
+                                  n_particles=n_particles,
+                                  df_min=df_min,
+                                  df_max=df_max,
+                                  df_diff_min=df_diff_min,
+                                  df_diff_max=df_diff_max,
+                                  df_ang_min=df_ang_min,
+                                  df_ang_max=df_ang_max,
+                                  kv=kv,
+                                  cs=cs,
+                                  ac=ac,
+                                  phase=phase,
+                                  bf=bf,
+                                  do_log=do_log
+                                  )
+
+        projs_f *= CTFs
+        projs_r = fourier.do_ifft(projs_f,d=2,batch=True)
+
+    if do_snr:
+        signal = np.std(projs_r)
+        noise = signal/snr
+        projs_r_noise = np.random.normal(loc=projs_r,scale=noise)
+    else:
+        projs_r_noise = projs_r
+
+    meta_data_df = pd.DataFrame({'N':N,'psize':psize,'snr':snr,'df1_A':df1s,'df2_A':df2s,'df_ang_deg':df_ang_deg,'kev':kv,'ac':ac,'cs_mm':cs,
+              'rotation_quaternion':[np.array2string(q) for q in qs]
+            })
+
+    return(projs_r,projs_r_noise,meta_data_df)
