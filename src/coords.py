8d6b1e58d267436f3c69ee0aec9ae7ec76f64103
Geoffrey Woollard <geoffwoollard@gmail.com>
2021-04-19 16:03:25 -0400

    uniform_rotations

--- src/coords.py
+++ src/coords.py
@@ -42,6 +42,6 @@ def get_random_quat(num_pts):
     return np.transpose(quat)
 
 def uniform_rotations(num):
-  qs = coords.get_random_quat(num)
-  Rs = coords.SO3.matrix_from_quaternion() # num,3,3
+  qs = get_random_quat(num)
+  Rs = SO3.matrix_from_quaternion() # num,3,3
   return(Rs)
