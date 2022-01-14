1d6c470700d7b873e86615a0d579457e01e67873
Harshit Gupta <hrshit786@gmail.com>
2022-01-14 04:41:57 -0800

    splitted utils from linear simulator

---
+++ src/distributions.py
@@ -0,0 +1,16 @@
+"""contain functions dealing with probability distributions."""
+
+import torch
+
+
+def uniform_to_triangular(x):
+    """Convert samples from uniform distribution to triangular distribution.
+    Parameters
+    ----------
+    x: torch.Tensor
+    Returns
+    -------
+    out: torch.Tensor
+    """
+    return (x - 0.5).sign() * (1 - (1 - (2 * x - 1).abs()).sqrt())
+
