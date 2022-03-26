64e33be0c90305ab72cb007b971e4dd6f1b024db
Geoffrey Woollard <geoffwoollard@gmail.com>
2022-03-25 22:31:19 -0700

    rename imports and use as to avoid black isort clash

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -1,8 +1,10 @@
 """Test functions for transforms."""
 
 import torch
-from compSPI.transforms import fourier_to_primal_2D, primal_to_fourier_2D
-from compSPI.transforms import fourier_to_primal_3D, primal_to_fourier_3D
+from compSPI.transforms import fourier_to_primal_2D as fft_2d
+from compSPI.transforms import fourier_to_primal_3D as fft_3d
+from compSPI.transforms import primal_to_fourier_2D as ifft_2d
+from compSPI.transforms import primal_to_fourier_3D as ifft_3d
 
 
 def test_primal_to_fourier_2D():
@@ -17,7 +19,7 @@ def test_primal_to_fourier_2D():
         im[0, 0, size // 2, size // 2] = 1
         im[1, 0, :, :] = 1
 
-        im_fourier = primal_to_fourier_2D(im)
+        im_fourier = fft_2d(im)
 
         expected_im_fourier = torch.zeros(2, 1, size, size)
         expected_im_fourier[0] = im[1] * im[0].sum()
@@ -41,7 +43,7 @@ def test_fourier_to_primal_2D():
         im_fourier[0, 0, size // 2, size // 2] = 1
         im_fourier[1, 0, :, :] = 1
 
-        im = fourier_to_primal_2D(im_fourier)
+        im = ifft_2d(im_fourier)
 
         expected_im = torch.zeros(2, 1, size, size)
         expected_im[0] = im_fourier[1] / size**2
@@ -63,7 +65,7 @@ def test_primal_to_fourier_3D():
         im[0, size // 2, size // 2, size // 2] = 1
         im[1, :, :, :] = 1
 
-        im_fourier = primal_to_fourier_3D(im)
+        im_fourier = fft_3d(im)
 
         expected_im_fourier = torch.zeros(2, size, size, size)
         expected_im_fourier[0] = im[1] * im[0].sum()
@@ -87,7 +89,7 @@ def test_fourier_to_primal_3D():
         im_fourier[0, size // 2, size // 2, size // 2] = 1
         im_fourier[1, :, :, :] = 1
 
-        im = fourier_to_primal_3D(im_fourier)
+        im = ifft_3d(im_fourier)
 
         expected_im = torch.zeros(2, size, size, size)
         expected_im[0] = im_fourier[1] / size**3
