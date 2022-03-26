6092d227abfc57d8d3dbe00d56426e39d4a74e20
Geoffrey Woollard <geoffwoollard@gmail.com>
2022-03-25 22:25:09 -0700

    black and isort are clashing. went with black, but split to two lines

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -1,8 +1,8 @@
 """Test functions for transforms."""
 
 import torch
-from compSPI.transforms import (fourier_to_primal_2D, fourier_to_primal_3D,
-                                primal_to_fourier_2D, primal_to_fourier_3D)
+from compSPI.transforms import fourier_to_primal_2D, primal_to_fourier_2D
+from compSPI.transforms import fourier_to_primal_3D, primal_to_fourier_3D
 
 
 def test_primal_to_fourier_2D():
