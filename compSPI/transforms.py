ff67a329f19edcfda85f8c0af89ac98f632c1e19
Geoffrey Woollard <geoffwoollard@gmail.com>
2022-03-25 21:50:45 -0700

    3d fft

--- compSPI/transforms.py
+++ compSPI/transforms.py
@@ -3,17 +3,17 @@ import torch
 
 
 def primal_to_fourier_2D(r):
-    """Return fourier transform of a batch of image.
+    """Return 2D fourier transform of a batch of images.
 
     Parameters
     ----------
     r: torch.Tensor
-        Tensor of size (batch,1, size,size)
+        Tensor of size (batch, 1, size, size)
 
     Returns
     -------
     out: torch.Tensor
-        Tensor of size (batch,1, size,size)
+        Tensor of size (batch, 1, size, size)
     """
     r = torch.fft.fftshift(r, dim=(-2, -1))
     return torch.fft.ifftshift(
@@ -22,7 +22,7 @@ def primal_to_fourier_2D(r):
 
 
 def fourier_to_primal_2D(f):
-    """Return inverse fourier transform of a batch of image.
+    """Return inverse 2D fourier transform of a batch of images.
 
     Parameters
     ----------
@@ -37,4 +37,42 @@ def fourier_to_primal_2D(f):
     f = torch.fft.ifftshift(f, dim=(-2, -1))
     return torch.fft.fftshift(
         torch.fft.ifftn(f, s=(f.shape[-2], f.shape[-1]), dim=(-2, -1)), dim=(-2, -1)
+    )
+
+
+def primal_to_fourier_3D(r):
+    """Return 3D fourier transform of a batch of images.
+
+    Parameters
+    ----------
+    r: torch.Tensor
+        Tensor of size (batch, size, size, size)
+    Returns
+    -------
+    out: torch.Tensor
+        Tensor of size (batch, size, size, size)
+    """
+    r = torch.fft.fftshift(r, dim=(-3, -2, -1))
+    return torch.fft.ifftshift(
+        torch.fft.fftn(r, s=(r.shape[-3], r.shape[-2], r.shape[-1]), dim=(-3, 2, -1)),
+        dim=(-3, -2, -1),
+    )
+
+
+def fourier_to_primal_3D(f):
+    """Return inverse 3D fourier transform of a batch of images.
+
+    Parameters
+    ----------
+    f: torch.Tensor
+        Tensor of size (batch, size, size, size)
+    Returns
+    -------
+    out: torch.Tensor
+        Tensor of size (batch, size, size, size)
+    """
+    f = torch.fft.ifftshift(f, dim=(-3, -2, -1))
+    return torch.fft.fftshift(
+        torch.fft.ifftn(f, s=(f.shape[-3], f.shape[-2], f.shape[-1]), dim=(-3, -2, -1)),
+        dim=(-3, -2, -1),
     )
