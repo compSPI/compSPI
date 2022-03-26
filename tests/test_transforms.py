903a5ff5bd327e6d23618991ff9c18a0ae975c6b
Geoffrey Woollard <geoffwoollard@gmail.com>
2022-03-25 23:43:33 -0700

    simplify imports

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -2,10 +2,7 @@
 
 import torch
 
-from compSPI.transforms import fourier_to_primal_2D as ifft_2d
-from compSPI.transforms import fourier_to_primal_3D as ifft_3d
-from compSPI.transforms import primal_to_fourier_2D as fft_2d
-from compSPI.transforms import primal_to_fourier_3D as fft_3d
+import compSPI.transforms
 
 
 def test_primal_to_fourier_2D():
@@ -20,7 +17,7 @@ def test_primal_to_fourier_2D():
         im[0, 0, size // 2, size // 2] = 1
         im[1, 0, :, :] = 1
 
-        im_fourier = fft_2d(im)
+        im_fourier = compSPI.transforms.primal_to_fourier_2D(im)
 
         expected_im_fourier = torch.zeros(2, 1, size, size)
         expected_im_fourier[0] = im[1] * im[0].sum()
@@ -45,7 +42,7 @@ def test_fourier_to_primal_2D():
         im_fourier[0, 0, size // 2, size // 2] = 1
         im_fourier[1, 0, :, :] = 1
 
-        im = ifft_2d(im_fourier)
+        im = compSPI.transforms.fourier_to_primal_2D(im_fourier)
 
         expected_im = torch.zeros(2, 1, size, size)
         expected_im[0] = im_fourier[1] / size**2
@@ -67,7 +64,7 @@ def test_primal_to_fourier_3D():
         im[0, size // 2, size // 2, size // 2] = 1
         im[1, :, :, :] = 1
 
-        im_fourier = fft_3d(im)
+        im_fourier = compSPI.transforms.primal_to_fourier_3D(im)
 
         expected_im_fourier = torch.zeros(2, size, size, size)
         expected_im_fourier[0] = im[1] * im[0].sum()
@@ -92,7 +89,7 @@ def test_fourier_to_primal_3D():
         im_fourier[0, size // 2, size // 2, size // 2] = 1
         im_fourier[1, :, :, :] = 1
 
-        im = ifft_3d(im_fourier)
+        im = compSPI.transforms.fourier_to_primal_3D(im_fourier)
 
         expected_im = torch.zeros(2, size, size, size)
         expected_im[0] = im_fourier[1] / size**3
